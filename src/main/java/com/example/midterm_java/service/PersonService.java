609bd54343f26d2071c5dc42216f5ddc81bd08ce
Soufiane Belmir <belmir.soufiane@gmail.com>
2020-10-17 13:53:40 -0500

    Initial

---
+++ src/main/java/com/example/midterm_java/service/PersonService.java
@@ -0,0 +1,30 @@
+package com.example.midterm_java.service;
+
+import com.example.midterm_java.dao.PersonDao;
+import com.example.midterm_java.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+   private final PersonDao personDao;
+   @Autowired
+
+   public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+   }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople()
+    {
+        return personDao.selectAllPeople();
+    }
+
+}
